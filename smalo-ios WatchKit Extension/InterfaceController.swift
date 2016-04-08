808ebdbae02dcdb0d195d472e230a7950562c4be
Tetsu Susaki <susaki@cosmoway.net>
2016-04-08 12:45:23 +0900

    BundleIdの修正、watchとiPhoneで相互にデータ通信をさせた

--- smalo-ios WatchKit Extension/InterfaceController.swift
+++ smalo-ios WatchKit Extension/InterfaceController.swift
@@ -8,16 +8,50 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController,WCSessionDelegate {
 
+    @IBOutlet var label: WKInterfaceLabel!
+    
+    
+    var wcSession = WCSession.defaultSession()
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
+        // check supported
+        if WCSession.isSupported() {
+            // get default session
+            wcSession = WCSession.defaultSession()
+            //  set delegate
+            wcSession.delegate = self
+            //  activate session
+            wcSession.activateSession()
+        } else {
+            self.label.setText("通信失敗")
+        }
         // Configure interface objects here.
     }
 
+    @IBAction func button() {
+        WKInterfaceDevice.currentDevice().playHaptic(WKHapticType.Click)
+        
+        let message = [ "NotificationOn" : "watch:OK" ]
+        
+        wcSession.sendMessage(message, replyHandler: { replyDict in }, errorHandler: { error in })
+        
+    }
+    
+    // get message from watch
+    func session(session: WCSession, didReceiveMessage message: [String: AnyObject], replyHandler: [String: AnyObject] -> Void) {
+        
+        if let parentMessage = message["parentOK"] as? String {
+            
+            label.setText(parentMessage)
+        }
+    }
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
--- smalo-ios/ViewController.swift
+++ smalo-ios/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WCSessionDelegate {
+    
+    var wcSession = WCSession.defaultSession()
+    
+    @IBOutlet weak var label: UILabel!
+    var i = 0
+    
+    // protcol NSCorder init
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    // UIViewController init override
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // check supported
+        if WCSession.isSupported() {
+            //  get default session
+            wcSession = WCSession.defaultSession()
+            // set delegate
+            wcSession.delegate = self
+            // activate session
+            wcSession.activateSession()
+        } else {
+            print("Not support WCSession")
+        }
+    }
+    
+    // get message from watch
+    func session(session: WCSession, didReceiveMessage message: [String: AnyObject], replyHandler: [String: AnyObject] -> Void) {
+        
+        if let watchMessage = message["NotificationOn"] as? String {
+            
+            label.text = "watchOK"
+            
+            let message = [ "parentOK" : "通信完了"]
+            
+            wcSession.sendMessage(message, replyHandler: { replyDict in }, errorHandler:  { error in })
+        }
     }
 
     override func didReceiveMemoryWarning() {
