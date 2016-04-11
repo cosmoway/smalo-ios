f203831e835e24f55eae0d7508fe45db9c476328
Sakano Takeru <sakano@cosmoway.net>
2016-04-11 18:05:23 +0900

    peripheral検出と接続できるようにしました。

--- smalo-ios/ViewController.swift
+++ smalo-ios/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralManager: CBCentralManager!
+    var peripheral: CBPeripheral!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        switch central.state {
+        case CBCentralManagerState.PoweredOff:
+            print(central.state)
+            self.centralManager.stopScan()
+        case CBCentralManagerState.PoweredOn:
+            self.centralManager.scanForPeripheralsWithServices(nil, options: nil)
+        default:
+            print(central.state)
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        print("発見したBLEデバイス\(peripheral)")
+        if peripheral.name == "smalo" {
+            self.peripheral = peripheral
+            self.centralManager.connectPeripheral(self.peripheral, options: nil)
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        print("接続成功！")
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        let services: NSArray = peripheral.services!
+        print("\(services.count)このサービス発見！　\(services)")
+        for obj in services {
+            if let service = obj as? CBService {
+                if service.UUID.isEqual(CBUUID(string: "9ada4c64-c941-46c2-9156-c39addd4f77c")) {
+                    peripheral.discoverCharacteristics(nil, forService: service)
+                }
+            }
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        let characteristics: NSArray = service.characteristics!
+        print("\(characteristics.count)個のキャラクタリスティックを発見！　\(characteristics)")
+    }
+    
+    func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        print("接続失敗・・・")
+    }
 
 }
 
