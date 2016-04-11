0c38f8b96a5dcaab7407c4f22963a3c051fd508b
Tetsu Susaki <susaki@cosmoway.net>
2016-04-11 17:18:47 +0900

    メッセージを送信して返事があるまで送信中と表示するようにした、BLE接続確認用の仮のボタンを配置、ボタンの文字が変わるよう変更

--- smalo-ios WatchKit Extension/InterfaceController.swift
+++ smalo-ios WatchKit Extension/InterfaceController.swift
@@ -10,15 +10,15 @@ import WatchKit
 import Foundation
 import WatchConnectivity
 
-
 class InterfaceController: WKInterfaceController,WCSessionDelegate {
 
     @IBOutlet var label: WKInterfaceLabel!
     @IBOutlet var label2: WKInterfaceLabel!
+    @IBOutlet var label3: WKInterfaceLabel!
     
     @IBOutlet var myGroup: WKInterfaceGroup!
     @IBOutlet var openButton: WKInterfaceButton!
-    
+    @IBOutlet var connectButton: WKInterfaceButton!
     
     var wcSession = WCSession.defaultSession()
     
@@ -38,7 +38,8 @@ class InterfaceController: WKInterfaceController,WCSessionDelegate {
             wcSession.activateSession()
         }
         
-        let message = [ "watchWake" : "watch:OK" ]
+        //鍵の状態の取得
+        let message = [ "getState" : "watch:OK" ]
         
         wcSession.sendMessage(message, replyHandler: { replyDict in }, errorHandler: { error in })
         
@@ -46,33 +47,72 @@ class InterfaceController: WKInterfaceController,WCSessionDelegate {
     }
 
     @IBAction func button() {
+        
+        //バイブレーション
         WKInterfaceDevice.currentDevice().playHaptic(WKHapticType.Click)
         
+        label.setText("要求送信中")
+        
+        //開閉要求
         let message = [ "watchOC" : "watchから" ]
         
         wcSession.sendMessage(message, replyHandler: { replyDict in }, errorHandler: { error in })
         
     }
     
-    // get message from watch
+    @IBAction func button2() {
+        WKInterfaceDevice.currentDevice().playHaptic(WKHapticType.Click)
+        
+        label3.setText("接続要求送信")
+        
+        //BLEの接続確認
+        let message = [ "connect" : "接続要求" ]
+        
+        wcSession.sendMessage(message, replyHandler: { replyDict in }, errorHandler: { error in })
+    }
+    
+    //iPhoneからメッセージを受け取る
     func session(session: WCSession, didReceiveMessage message: [String: AnyObject], replyHandler: [String: AnyObject] -> Void) {
         
-        if let parentMessage = message["parentWake"] as? String {
+        //鍵が閉まってる場合
+        if let parentMessage = message["parentWakeClose"] as? String {
+            
+            openButton.setTitle("Open")
             
             label2.setText("通信完了")
             label.setText(parentMessage)
         }
         
-        if let parentMessage = message["parentOpen"] as? String {
+        //鍵が開いている場合
+        if let parentMessage = message["parentWakeOpen"] as? String {
             
-            myGroup.setBackgroundColor(UIColor.cyanColor())
+            openButton.setTitle("Close")
+            
+            label2.setText("通信完了")
             label.setText(parentMessage)
         }
         
+        //解錠メッセージを受け取る
+        if let parentMessage = message["parentOpen"] as? String {
+            
+            myGroup.setBackgroundColor(UIColor.blackColor())
+            label.setText(parentMessage)
+            openButton.setTitle("Close")
+        }
+        
+        //施錠メッセージを受け取る
         if let parentMessage = message["parentClose"] as? String {
             
             myGroup.setBackgroundColor(UIColor.blueColor())
             label.setText(parentMessage)
+            openButton.setTitle("Open")
+        }
+        
+        //BLE接続のメッセージを受け取る
+        if let parentMessage = message["BLE"] as? String {
+            
+            myGroup.setBackgroundColor(UIColor.blueColor())
+            label3.setText(parentMessage)
         }
     }
     
--- smalo-ios/ViewController.swift
+++ smalo-ios/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import WatchConnectivity
+import CoreBluetooth
 
-class ViewController: UIViewController,WCSessionDelegate {
+class ViewController: UIViewController,WCSessionDelegate, CBCentralManagerDelegate {
     
     var wcSession = WCSession.defaultSession()
     let open = 0 , close = 1
     var state = 1
+    var centralManager: CBCentralManager!
     
     @IBOutlet weak var label: UILabel!
     
@@ -42,29 +44,66 @@ class ViewController: UIViewController,WCSessionDelegate {
         } else {
             print("Not support WCSession")
         }
+        self.centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
     }
     
-    // get message from watch
+    // 1-2. CentralManager状態の受信
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        
+        switch (central.state) {
+        case .PoweredOff:
+            print("BLE PoweredOff")
+        case .PoweredOn:
+            print("BLE PoweredOn")
+            // 2-1. Peripheral探索開始
+            central.scanForPeripheralsWithServices(nil, options: nil)
+            /* ↑の第1引数はnilは非推奨。
+            該当サービスのCBUUIDオブジェクトの配列が望ましい */
+            let message = [ "BLE" : "CM状態受信完了"]
+            
+            wcSession.sendMessage(message, replyHandler: { replyDict in }, errorHandler:  { error in })
+            
+        case .Resetting:
+            print("BLE Resetting")
+        case .Unauthorized:
+            print("BLE Unauthorized")
+        case .Unknown:
+            print("BLE Unknown")
+        case .Unsupported:
+            print("BLE Unsupported")
+        }
+    }
+    
+    // watchからのメッセージを受け取る
     func session(session: WCSession, didReceiveMessage message: [String: AnyObject], replyHandler: [String: AnyObject] -> Void) {
         
-        if let watchMessage = message["watchWake"] as? String {
+        //鍵の状態の取得要求
+        if let watchMessage = message["getState"] as? String {
             
             label.text = watchMessage
             
             if( state == open ){
                 
-                let message = [ "parentWake" : "閉まっています"]
+                let message = [ "parentWakeClose" : "閉まっています"]
             
                 wcSession.sendMessage(message, replyHandler: { replyDict in }, errorHandler:  { error in })
                 
             }else if( state == close ){
                 
-                let message = [ "parentWake" : "開いています"]
+                let message = [ "parentWakeOpen" : "開いています"]
                 
                 wcSession.sendMessage(message, replyHandler: { replyDict in }, errorHandler:  { error in })
             }
+            let notification = UILocalNotification()
+            notification.fireDate = NSDate()	// すぐに通知したいので現在時刻を取得
+            notification.timeZone = NSTimeZone.defaultTimeZone()
+            notification.alertBody = "通信完了"
+            notification.alertAction = "OK"
+            notification.soundName = UILocalNotificationDefaultSoundName
+            UIApplication.sharedApplication().presentLocalNotificationNow(notification)
         }
         
+        //鍵の開閉要求
         if let watchMessage = message["watchOC"] as? String {
             if( state == close ){
                 label.text = watchMessage + "解錠"
@@ -84,6 +123,11 @@ class ViewController: UIViewController,WCSessionDelegate {
                 
                 state = close
             }
+        }
+        
+        //BLEの接続要求
+        if let watchMessage = message["connect"] as? String {
+            //ここでEdisonとBLE接続できるかどうかを返す
         }
         
     }
