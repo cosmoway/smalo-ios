d0463461c2d18026c81b68076a2826bf94a8284e
Sakano Takeru <sakano@cosmoway.net>
2016-04-15 17:19:19 +0900

    デザインをチェリーピックしました。

---
+++ smalo-ios/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  smalo-ios
+//
+//  Created by 坂野健 on 2016/04/19.
+//  Copyright © 2016年 須崎鉄. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+#import <CommonCrypto/CommonDigest.h>
+
+
+#endif /* Bridging_Header_h */
--- smalo-ios/ViewController.swift
+++ smalo-ios/ViewController.swift
@@ -9,17 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var keyFlag = true
+    var major: String?
+    var mainor: String?
+    let UUID: String = "\(UIDevice.currentDevice().identifierForVendor!.UUIDString)"
+    @IBOutlet weak var keyButton: UIButton!
+    @IBOutlet weak var gradationView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //グラデーションの開始色
+        let topColor = UIColor(red:0.16, green:0.68, blue:0.76, alpha:1)
+        //グラデーションの開始色
+        let bottomColor = UIColor(red:0.57, green:0.84, blue:0.88, alpha:1)
+        
+        //グラデーションの色を配列で管理
+        let gradientColors: [CGColor] = [topColor.CGColor, bottomColor.CGColor]
+        
+        //グラデーションレイヤーを作成
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        
+        //グラデーションの色をレイヤーに割り当てる
+        gradientLayer.colors = gradientColors
+        //グラデーションレイヤーをスクリーンサイズにする
+        gradientLayer.frame.size = self.gradationView.frame.size
+        
+        //グラデーションレイヤーをビューの一番下に配置
+        self.gradationView.layer.insertSublayer(gradientLayer, atIndex: 0)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func localNotification(msg: String) {
+        if UIApplication.sharedApplication().applicationState != UIApplicationState.Active {
+            
+            let notification = UILocalNotification()
+            notification.timeZone = NSTimeZone.defaultTimeZone()
+            notification.alertBody = msg
+            notification.soundName = UILocalNotificationDefaultSoundName
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        }
+    }
+    
+    @IBAction func keyButton(sender: AnyObject) {
+        if keyFlag {
+        } else {
+        }
+    }
+}
+extension String {
+    var sha256: String! {
+        return self.cStringUsingEncoding(NSUTF8StringEncoding).map { cstr in
+            var chars = [UInt8](count: Int(CC_SHA256_DIGEST_LENGTH), repeatedValue: 0)
+            CC_SHA256(
+                cstr,
+                CC_LONG(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)),
+                &chars
+            )
+            return chars.map { String(format: "%02X", $0) }.reduce("", combine: +)
+        }
+    }
 }
 
