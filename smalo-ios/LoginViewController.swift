81fc184be3ce06232efedea62b05bb2ecb9759c2
Sakano Takeru <sakano@cosmoway.net>
2016-05-09 15:27:29 +0900

    アイコンを変更いたしました。

---
+++ smalo-ios/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  smalo-ios
+//
+//  Created by 坂野健 on 2016/05/09.
+//  Copyright © 2016年 須崎鉄. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
