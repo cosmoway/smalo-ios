e536ff1112b67d4a6acd7d992ecec18104d4c6c2
Sakano Takeru <sakano@cosmoway.net>
2016-05-11 09:09:47 +0900

    サーバーにuuidと名前を送信出来るようにしました。

--- smalo-ios/LoginViewController.swift
+++ smalo-ios/LoginViewController.swift
@@ -12,6 +12,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userNameText: UITextField!
     @IBOutlet weak var userNameSendButton: UIButton!
     let gradientLayer = CAGradientLayer()
+    let UUID: String = "\(UIDevice.currentDevice().identifierForVendor!.UUIDString)"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,14 +34,74 @@ class LoginViewController: UIViewController {
         //グラデーションレイヤーをビューの一番下に配置
         self.view.layer.insertSublayer(gradientLayer, atIndex: 0)
     }
-    @IBAction func sendUserName(sender: AnyObject) {
-        performSegueWithIdentifier("showMain", sender: nil)
-        let ud = NSUserDefaults.standardUserDefaults()
-        ud.setBool(true, forKey: "isLogin")
+    @IBAction func sendUserName(sender:
+        AnyObject) {
+        if userNameText.text != "" {
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            //短いタイムアウト
+            config.timeoutIntervalForRequest = 20
+            //長居タイムアウト
+            config.timeoutIntervalForResource = 30
+            let session = NSURLSession(configuration: config)
+            
+            let urlString = "https://smalo.cosmoway.net:8443/api/v1/devices"
+            let request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+            
+            // set the method(HTTP-POST)
+            request.HTTPMethod = "POST"
+            // set the header(s)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            // set the request-body(JSON)
+            let params: [String: AnyObject] = [
+                "uuid" : UUID,
+                "name" : userNameText.text!
+            ]
+            do {
+                request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: NSJSONWritingOptions.init(rawValue: 2))
+            } catch {
+                print("NSJSONSerialization Error")
+                return
+            }
+            
+            // use NSURLSessionDataTask
+            let task = session.dataTaskWithRequest(request, completionHandler: { data, response, error in
+                if (error == nil) {
+                    let result = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+                    switch (result) {
+                    case "204":
+                        dispatch_async(dispatch_get_main_queue(), {
+                            let ud = NSUserDefaults.standardUserDefaults()
+                            ud.setBool(false, forKey: "isLogin")
+                            self.performSegueWithIdentifier("showMain", sender: nil)
+                            print("登録されたよ")
+                        })
+                        break
+                    case "400 Bad Request":
+                       
+                        break
+                    case "403 Forbidden":
+                        
+                        break
+                    default:
+                        break
+                    }
+                    print(result)
+                } else {
+                    print(error)
+                }
+            })
+            task.resume()
+        }
+    }
+    @IBAction func tapScreen(sender: AnyObject) {
+        self.view.endEditing(true)
     }
     
     override func viewDidLayoutSubviews() {
-        userNameText.attributedPlaceholder = NSAttributedString(string: "ユーザーネーム", attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        userNameText.attributedPlaceholder = NSAttributedString(string: "名前を入力してください。", attributes: [NSForegroundColorAttributeName: UIColor(colorLiteralRed: 1, green: 1, blue: 1, alpha: 1)])
+        userNameText.textColor = UIColor(colorLiteralRed: 1, green: 1, blue: 1, alpha: 1)
+        userNameText.backgroundColor = UIColor(colorLiteralRed: 1, green: 1, blue: 1, alpha: 0.25)
         userNameSendButton.layer.borderColor = UIColor.whiteColor().CGColor
         userNameSendButton.layer.borderWidth = 1.0
     }
